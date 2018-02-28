959139b94b4d318715f6e34ee520ebd5401d199c
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-02-28 04:41:10 +0200

    Remove HSStudioNEOExtSave code

--- LightManager/LightManagerSaveLoadHandler.cs
+++ LightManager/LightManagerSaveLoadHandler.cs
@@ -6,7 +6,6 @@ using Studio;
 using IllusionUtility.GetUtility;
 using System.Collections;
 using UnityEngine;
-using HSStudioNEOExtSave;
 
 namespace LightManager
 {
@@ -19,19 +18,14 @@ namespace LightManager
         const string TARGET_NAME = "TargetName";
         const string ROTATION_SPEED = "RotationSpeed";
 
-        LightManagerSaveLoadHandler handler;
-
         void Start()
         {
             HSExtSave.HSExtSave.RegisterHandler(MOD_NAME, null, null, OnLoad, null, OnSave, null, null);
-            handler = new LightManagerSaveLoadHandler();
-            StudioNEOExtendSaveMgr.Instance.RegisterHandler(handler);
         }
 
         void OnDestroy()
         {
             HSExtSave.HSExtSave.UnregisterHandler(MOD_NAME);
-            StudioNEOExtendSaveMgr.Instance.Unregisterhandler(handler);
         }
 
         void OnLoad(string path, XmlNode node)
@@ -109,65 +103,6 @@ namespace LightManager
                 objectList.ForEach(x => x.WriteTo(xmlWriter));
                 xmlWriter.WriteEndElement();
             }
-        }
-    }
-
-    class LightManagerSaveLoadHandler : StudioNEOExtendSaveLoadHandler
-    {
-        const string MOD_NAME = "LightManager";
-        const string ELEMENT_NAME = "Light";
-        const string LIGHT_ID = "LightID";
-        const string TARGET_ID = "TargetID";
-        const string TARGET_NAME = "TargetName";
-        const string ROTATION_SPEED = "RotationSpeed";
-
-        public int priority { get; set; }
-
-        public void OnLoad(XmlElement rootElement)
-        {
-            try
-            {
-                var xElement = XElement.Parse(rootElement.OuterXml);
-                var mainElement = xElement.Element(MOD_NAME);
-                if(mainElement != null)
-                {
-                    foreach(var element in mainElement.Elements())
-                    {
-                        ObjectCtrlInfo lightInfo;
-                        if(Studio.Studio.Instance.dicObjectCtrl.TryGetValue((int)element.Element(LIGHT_ID), out lightInfo))
-                        {
-                            if(lightInfo is OCILight)
-                            {
-                                ObjectCtrlInfo targetInfo;
-                                if(Studio.Studio.Instance.dicObjectCtrl.TryGetValue((int)element.Element(TARGET_ID), out targetInfo))
-                                {
-                                    var ocilight = lightInfo as OCILight;
-                                    var light = (lightInfo as OCILight).light;
-                                    var tracker = light.gameObject.AddComponent<TrackTransform>();
-
-                                    var chara = (targetInfo as OCIChar).charInfo;
-                                    string prefix = chara is CharFemale ? "cf" : "cm";
-                                    tracker.target = chara.chaBody.transform.FindLoop(prefix + "_J_Mune00").transform;
-                                    tracker.targetKey = targetInfo.objectInfo.dicKey;
-                                    tracker.rotationSpeed = (float?)element.Element(ROTATION_SPEED) ?? 1f;
-                                    tracker.targetName = (string)element.Element(TARGET_NAME) ?? "Blank";
-                                }
-                            }
-                        }
-                    }
-
-                    Console.WriteLine("Load LightManager savedata from XML");
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-
-        public void OnSave(XmlElement rootElement)
-        {
-            return;
         }
     }
 }
