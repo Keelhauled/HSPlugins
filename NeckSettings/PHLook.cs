6b60b489f4abc396d0ef3188723009a2f5aba72e
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-04-16 21:32:48 +0300

    NeckSettings PHLook

---
+++ NeckSettings/PHLook.cs
@@ -0,0 +1,32 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Reflection;
+
+namespace NeckSettings
+{
+    static class PHLook
+    {
+        [HarmonyPatch(typeof(NeckLookCalcVer2))]
+        [HarmonyPatch("Init")]
+        static class HarmonyPatch_NeckLookCalcVer2_Init
+        {
+            static void Postfix(NeckLookCalcVer2 __instance)
+            {
+                __instance.calcLerp = 0.8f;
+
+                foreach(var item in __instance.neckTypeStates)
+                {
+                    if(item.lookType == NECK_LOOK_TYPE_VER2.TARGET)
+                    {
+                        item.limitBreakCorrectionValue = 89f;
+                        item.leapSpeed = 3f;
+                    }
+                }
+            }
+        }
+    }
+}
