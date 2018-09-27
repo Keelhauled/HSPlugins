9169f3a094098e83cdac559343741a2ae9b784a7
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-09-27 21:41:42 +0300

    TitleShortcuts

---
+++ TitleShortcuts/TitleShortcuts.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TitleShortcuts
+{
+    class TitleShortcuts : MonoBehaviour
+    {
+        bool autoStart = true;
+        bool check = false;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void OnLevelWasLoaded(int level)
+        {
+            StartInput();
+        }
+
+        void StartInput()
+        {
+            if(SceneManager.GetActiveScene().name == "Title")
+            {
+                if(!check)
+                {
+                    check = true;
+                    StartCoroutine(InputCheck());
+                }
+            }
+            else
+            {
+                check = false;
+            }
+        }
+
+        IEnumerator InputCheck()
+        {
+            while(check)
+            {
+                if(!Manager.Scene.Instance.IsNowLoadingFade)
+                {
+                    if(Input.GetKeyDown(KeyCode.N))
+                    {
+                        OnCustomFemale();
+                    }
+                    else if(Input.GetKeyDown(KeyCode.M))
+                    {
+                        OnCustomMale();
+                    }
+                    else if(autoStart)
+                    {
+                        OnCustomFemale();
+                    }
+                }
+
+                yield return null;
+            }
+        }
+
+        void OnCustomMale()
+        {
+            Singleton<TitleScene>.Instance.SelectCustomM();
+            check = false;
+        }
+
+        void OnCustomFemale()
+        {
+            Singleton<TitleScene>.Instance.SelectCustomF();
+            check = false;
+        }
+    }
+}
---
+++ TitleShortcuts/TitleShortcutsPlugins.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using IllusionPlugin;
+
+namespace TitleShortcuts
+{
+    public class TitleShortcutsPlugins : IEnhancedPlugin
+    {
+        public const string PLUGIN_NAME = "TitleShortcuts";
+        public const string PLUGIN_VERSION = "1.0";
+        public string Name => PLUGIN_NAME;
+        public string Version => PLUGIN_VERSION;
+
+        public string[] Filter => new string[]
+        {
+            "HoneySelect_32",
+            "HoneySelect_64",
+        };
+
+        public void OnApplicationStart()
+        {
+            new GameObject(PLUGIN_NAME).AddComponent<TitleShortcuts>();
+        }
+
+        public void OnUpdate(){}
+        public void OnLateUpdate(){}
+        public void OnApplicationQuit(){}
+        public void OnLevelWasInitialized(int level){}
+        public void OnLevelWasLoaded(int level){}
+        public void OnFixedUpdate(){}
+    }
+}
