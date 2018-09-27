978586189aa2177a988534129a5bec9badc6fc0b
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-09-27 23:50:18 +0300

    Rename projects 2

--- ModMenuManager/Properties/AssemblyInfo.cs
+++ ModMenuManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModMenuManager_IPlugin")]
+[assembly: AssemblyTitle("ModMenuManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModMenuManager_IPlugin")]
+[assembly: AssemblyProduct("ModMenuManager")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
