d5c018c268ec176d03324cf60175807a79011e87
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2018-09-27 23:55:25 +0300

    Rename projects 3

--- HideUI/Properties/AssemblyInfo.cs
+++ HideUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HideUI_IPlugin")]
+[assembly: AssemblyTitle("HideUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HideUI_IPlugin")]
+[assembly: AssemblyProduct("HideUI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
